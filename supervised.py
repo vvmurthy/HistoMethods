99f9f460d7f425da452db78de15691050a1808e0
vmurthy800 <murthyveda0@gmail.com>
2016-12-13 01:34:50 -0500

    Net runs; loss does not change

--- supervised.py
+++ supervised.py
@@ -101,21 +101,24 @@ def create_dataset(images):
                 data = np.concatenate((add,data))
                 label_vector = np.expand_dims(np.array([0,0,0,1]),axis=0)
                 labels = np.concatenate((label_vector,labels))
-        
+     
+                
+    labels = labels.astype(np.int32)
     return data, labels
     
     
 def data_split(data,labels):
+    train_data = np.zeros((0,3,27,27)).astype(np.float32)
+    train_labels = np.zeros((0,4)).astype(np.int8)
+    val_data = np.zeros((0,3,27,27)).astype(np.float32)
+    val_labels = np.zeros((0,4)).astype(np.int8)
+    test_data = np.zeros((0,3,27,27)).astype(np.float32)
+    test_labels = np.zeros((0,4)).astype(np.int8)
     for n in range(0,labels.shape[0]):
+        
         im = np.expand_dims(data[n,:,:,:],axis=0)
         lab = np.expand_dims(labels[n,:],axis=0)
         splitr = np.random.random();
-        train_data = np.zeros((0,3,27,27)).astype(np.float32)
-        train_labels = np.zeros((0,4)).astype(np.int8)
-        val_data = np.zeros((0,3,27,27)).astype(np.float32)
-        val_labels = np.zeros((0,4)).astype(np.int8)
-        test_data = np.zeros((0,3,27,27)).astype(np.float32)
-        test_labels = np.zeros((0,4)).astype(np.int8)
         
         if(splitr < 0.2):
             test_data = np.concatenate((test_data,im))
@@ -126,7 +129,7 @@ def data_split(data,labels):
         else:
             train_data = np.concatenate((train_data,im))
             train_labels = np.concatenate((train_labels,lab))
-        return train_data, train_labels, val_data, val_labels, test_data, test_labels
+    return train_data, train_labels, val_data, val_labels, test_data, test_labels
         
     
 def check_coordinates(images):
@@ -149,27 +152,30 @@ def build_cnn(input_var=None):
 
     network = lasagne.layers.InputLayer(shape=(None, 3, 27, 27),
     input_var=input_var)
+    print(lasagne.layers.get_output_shape(network))
 
     network = lasagne.layers.Conv2DLayer(
     network, num_filters=32, filter_size=(5, 5),
-    nonlinearity=lasagne.nonlinearities.rectify,
-    W=lasagne.init.GlorotUniform())
+    nonlinearity=lasagne.nonlinearities.rectify)
+    print(lasagne.layers.get_output_shape(network))
 
     network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
+    print(lasagne.layers.get_output_shape(network))
 
     network = lasagne.layers.Conv2DLayer(
     network, num_filters=32, filter_size=(5, 5),
     nonlinearity=lasagne.nonlinearities.rectify)
+    print(lasagne.layers.get_output_shape(network))
 
-    network = lasagne.layers.DenseLayer(
-    lasagne.layers.dropout(network, p=.5),
+    network = lasagne.layers.DenseLayer(network,
     num_units=256,
     nonlinearity=lasagne.nonlinearities.rectify)
+    print(lasagne.layers.get_output_shape(network))
 
-    network = lasagne.layers.DenseLayer(
-    lasagne.layers.dropout(network, p=.5),
+    network = lasagne.layers.DenseLayer(network,
     num_units=4,
     nonlinearity=lasagne.nonlinearities.softmax)
+    print(lasagne.layers.get_output_shape(network))
     return network
 
 def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
@@ -177,6 +183,11 @@ def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
     if shuffle:
         indices = np.arange(len(inputs))
         np.random.shuffle(indices)
+        
+    if(batchsize > len(targets)):
+        yield inputs, targets
+        
+        
     for start_idx in range(0, len(inputs) - batchsize + 1, batchsize):
         if shuffle:
             excerpt = indices[start_idx:start_idx + batchsize]
@@ -191,28 +202,27 @@ def main():
     X_train, y_train, X_val, y_val, X_test, y_test = data_split(data,labels)
     #Create training functions in theano
     input_var = T.tensor4('inputs')
-    target_var = T.ivector('targets')
+    target_var = T.imatrix('targets')
     network = build_cnn(input_var)
     
     prediction = lasagne.layers.get_output(network)
+    prediction = T.clip(prediction,0.00000001,0.99999999)
     loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
     loss = loss.mean()
 
     params = lasagne.layers.get_all_params(network, trainable=True)
     updates = lasagne.updates.nesterov_momentum(
-    loss, params, learning_rate=0.01, momentum=0.9)
+    loss, params, learning_rate=0.001, momentum=0.9)
 
     test_prediction = lasagne.layers.get_output(network, deterministic=True)
+    test_prediction = T.clip(prediction,0.00000001,0.99999999)
     test_loss = lasagne.objectives.categorical_crossentropy(test_prediction,
     target_var)
     test_loss = test_loss.mean()
 
-    test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var),
-    dtype=theano.config.floatX)
+    train_fn = theano.function([input_var, target_var], [loss,prediction], updates=updates)
 
-    train_fn = theano.function([input_var, target_var], loss, updates=updates)
-
-    val_fn = theano.function([input_var, target_var], [test_loss, test_acc])
+    val_fn = theano.function([input_var, target_var], [test_loss, test_prediction])
     
     # Finally, launch the training loop.
     print("Starting training...")
@@ -220,12 +230,14 @@ def main():
     for epoch in range(num_epochs):
         # In each epoch, we do a full pass over the training data:
         train_err = 0
+        rd_err = 0
         train_batches = 0
      
-        for batch in iterate_minibatches(X_train, y_train, 500, shuffle=True):
+        for batch in iterate_minibatches(X_train, y_train, 100, shuffle=True):
             inputs, targets = batch
-            train_err += train_fn(inputs, targets)
+            rd_err, preds = train_fn(inputs, targets)
             train_batches += 1
+            train_err += rd_err
             
             if(train_batches % 10 == 0):
                 print("Completed training minibatch {}".format(train_batches))
@@ -233,35 +245,26 @@ def main():
             
         # And a full pass over the validation data:
         val_err = 0
-        val_acc = 0
         val_batches = 0
-        for batch in iterate_minibatches(X_val, y_val, 500, shuffle=False):
+        for batch in iterate_minibatches(X_val, y_val, 100, shuffle=False):
             inputs, targets = batch
-            err, acc = val_fn(inputs, targets)
+            err, preds = val_fn(inputs, targets)
             val_err += err
-            val_acc += acc
             val_batches += 1
         # Then we print the results for this epoch:
         print("Epoch {} of {} Completed".format(
         epoch + 1, num_epochs))
         print(" training loss:\t\t{:.6f}".format(train_err / train_batches))
         print(" validation loss:\t\t{:.6f}".format(val_err / val_batches))
-        print(" validation accuracy:\t\t{:.2f} %".format(
-        val_acc / val_batches * 100))
         # After training, we compute and print the test error:
         test_err = 0
-        test_acc = 0
         test_batches = 0
-        for batch in iterate_minibatches(X_test, y_test, 500, shuffle=False):
+        for batch in iterate_minibatches(X_test, y_test, 100, shuffle=False):
             inputs, targets = batch
-            err, acc = val_fn(inputs, targets)
+            err, preds = val_fn(inputs, targets)
             test_err += err
-            test_acc += acc
             test_batches += 1
         print("Final results:")
         print(" test loss:\t\t\t{:.6f}".format(test_err / test_batches))
-        print(" test accuracy:\t\t{:.2f} %".format(
-        test_acc / test_batches * 100))
-
 main()
 
