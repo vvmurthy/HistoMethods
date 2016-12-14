499564c8b68a534faea994899538b63a3a65f881
vmurthy800 <murthyveda0@gmail.com>
2016-12-14 01:36:54 -0500

    Mean-centered ddata, Validation loss does not change.

--- supervised.py
+++ supervised.py
@@ -28,11 +28,16 @@ def read_data():
 "Labels are in the form [epithelial, fibroblast, inflammatory, other]"
 def create_dataset(images):
     data = np.zeros((0,3,27,27)).astype(np.float32)
-    labels = np.zeros((0,4)).astype(np.int8)
+    labels = np.zeros((0,1)).astype(np.int8)
     r = 13
     
     #take 1 500x500 image
     for k in range(0,100):
+        
+        if(k % 10 == 0):
+            print("Loading Image " + str(k) + " of 100")
+        
+        
         image = images[k].astype(np.float32)
         image = np.swapaxes(np.swapaxes(image, 1, 2), 0, 1)
      
@@ -43,14 +48,15 @@ def create_dataset(images):
         if(coors.shape[0] > 0):
             for i in range(0,coors.size/2):
                 y = np.min((500-14,coors[i,0]))
-                y = np.max((13,y))
+                y = np.max((13,y)).astype(np.int32)
                 x = np.min((500-14,coors[i,1]))
-                x = np.max((13,x))
+                x = np.max((13,x)).astype(np.int32)
                 add = np.expand_dims(image[:,x-r:x+r+1,y-r:y+r+1],axis=0)
-                print(str(x) + " " + str(y) + " " + str(add.shape) + " " + str(data.shape))
                 data = np.concatenate((add,data))
-                label_vector = np.expand_dims(np.array([0,1,0,0]),axis=0)
-                labels = np.concatenate((label_vector,labels))
+                labels = np.append(labels,1)
+                
+                #label_vector = np.expand_dims(np.array([0,1,0,0]),axis=0)
+              #  labels = np.concatenate((label_vector,labels),axis=0)
             
         epithelial = sio.loadmat("/home/veda/ColonHistology/CRCHistoPhenotypes_2016_04_28/Classification/img" + str(k+1) + "/img" + 
                                  str(k+1) +"_epithelial")
@@ -58,14 +64,15 @@ def create_dataset(images):
         if(coors.shape[0] > 0):
             for i in range(0,coors.size/2):
                 y = np.min((500-14,coors[i,0]))
-                y = np.max((13,y))
+                y = np.max((13,y)).astype(np.int32)
                 x = np.min((500-14,coors[i,1]))
-                x = np.max((13,x))
+                x = np.max((13,x)).astype(np.int32)
                 add = np.expand_dims(image[:,x-r:x+r+1,y-r:y+r+1],axis=0)
-                print(str(x) + " " + str(y) + " " + str(add.shape) + " " + str(data.shape))
                 data = np.concatenate((add,data))
-                label_vector = np.expand_dims(np.array([1,0,0,0]),axis=0)
-                labels = np.concatenate((label_vector,labels))
+                #label_vector = np.expand_dims(np.array([0]),axis=0)
+                labels = np.append(labels,0)
+                #label_vector = np.expand_dims(np.array([1,0,0,0]),axis=0)
+               # labels = np.concatenate((label_vector,labels),axis=0)
             
         inflammatory = sio.loadmat("/home/veda/ColonHistology/CRCHistoPhenotypes_2016_04_28/Classification/img" + str(k+1) + "/img" + 
                                  str(k+1) +"_inflammatory")
@@ -74,14 +81,15 @@ def create_dataset(images):
         if(coors.shape[0] > 0):
             for i in range(0,coors.size/2):
                 y = np.min((500-14,coors[i,0]))
-                y = np.max((13,y))
+                y = np.max((13,y)).astype(np.int32)
                 x = np.min((500-14,coors[i,1]))
-                x = np.max((13,x))
+                x = np.max((13,x)).astype(np.int32)
                 add = np.expand_dims(image[:,x-r:x+r+1,y-r:y+r+1],axis=0)
-                print(str(x) + " " + str(y) + " " + str(add.shape) + " " + str(data.shape))
                 data = np.concatenate((add,data))
-                label_vector = np.expand_dims(np.array([0,0,1,0]),axis=0)
-                labels = np.concatenate((label_vector,labels))
+                #label_vector = np.expand_dims(np.array([2]),axis=0)
+                labels = np.append(labels,2)
+                #label_vector = np.expand_dims(np.array([0,0,1,0]),axis=0)
+                #labels = np.concatenate((label_vector,labels),axis=0)
                 
           
         others = sio.loadmat("/home/veda/ColonHistology/CRCHistoPhenotypes_2016_04_28/Classification/img" + str(k+1) + "/img" + 
@@ -89,18 +97,19 @@ def create_dataset(images):
         coors = others["detection"]
         if(coors.shape[0] > 0):
             for i in range(0,coors.size/2):
-                y = coors[i,0]
-                x = coors[i,1]
+                y = coors[i,0].astype(np.int32)
+                x = coors[i,1].astype(np.int32)
                 
                 y = np.min((500-14,coors[i,0]))
-                y = np.max((13,y))
+                y = np.max((13,y)).astype(np.int32)
                 x = np.min((500-14,coors[i,1]))
-                x = np.max((13,x))
+                x = np.max((13,x)).astype(np.int32)
                 add = np.expand_dims(image[:,x-r:x+r+1,y-r:y+r+1],axis=0)
-                print(str(x) + " " + str(y) + " " + str(add.shape) + " " + str(data.shape))
                 data = np.concatenate((add,data))
-                label_vector = np.expand_dims(np.array([0,0,0,1]),axis=0)
-                labels = np.concatenate((label_vector,labels))
+                #label_vector = np.expand_dims(np.array([3]),axis=0)
+                labels = np.append(labels,3)
+                #label_vector = np.expand_dims(np.array([0,0,0,1]),axis=0)
+                #labels = np.concatenate((label_vector,labels),axis=0)
      
                 
     labels = labels.astype(np.int32)
@@ -109,26 +118,32 @@ def create_dataset(images):
     
 def data_split(data,labels):
     train_data = np.zeros((0,3,27,27)).astype(np.float32)
-    train_labels = np.zeros((0,4)).astype(np.int8)
+    train_labels = np.zeros((0,1)).astype(np.int8)
     val_data = np.zeros((0,3,27,27)).astype(np.float32)
-    val_labels = np.zeros((0,4)).astype(np.int8)
+    val_labels = np.zeros((0,1)).astype(np.int8)
     test_data = np.zeros((0,3,27,27)).astype(np.float32)
-    test_labels = np.zeros((0,4)).astype(np.int8)
+    test_labels = np.zeros((0,1)).astype(np.int8)
     for n in range(0,labels.shape[0]):
         
+        if(n % 5000 == 0):
+            print("Splitting Image " + str(n) + " of " + str(labels.shape[0]))
+        
         im = np.expand_dims(data[n,:,:,:],axis=0)
-        lab = np.expand_dims(labels[n,:],axis=0)
+        lab = labels[n]
         splitr = np.random.random();
         
         if(splitr < 0.2):
             test_data = np.concatenate((test_data,im))
-            test_labels = np.concatenate((test_labels,lab))
+            test_labels = np.append(test_labels,lab)
+            #test_labels = np.concatenate((test_labels,lab))
         elif(splitr > 0.85):
             val_data = np.concatenate((val_data,im))
-            val_labels = np.concatenate((val_labels,lab))
+            val_labels = np.append(val_labels,lab)
+            #val_labels = np.concatenate((val_labels,lab))
         else:
             train_data = np.concatenate((train_data,im))
-            train_labels = np.concatenate((train_labels,lab))
+            train_labels = np.append(train_labels,lab)
+            #train_labels = np.concatenate((train_labels,lab))
     return train_data, train_labels, val_data, val_labels, test_data, test_labels
         
     
@@ -155,16 +170,23 @@ def build_cnn(input_var=None):
     print(lasagne.layers.get_output_shape(network))
 
     network = lasagne.layers.Conv2DLayer(
-    network, num_filters=32, filter_size=(5, 5),
+    network, num_filters=32, filter_size=(3, 3),
     nonlinearity=lasagne.nonlinearities.rectify)
     print(lasagne.layers.get_output_shape(network))
 
-    network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
-    print(lasagne.layers.get_output_shape(network))
-
+    
     network = lasagne.layers.Conv2DLayer(
-    network, num_filters=32, filter_size=(5, 5),
+    network, num_filters=32, filter_size=(3, 3),
     nonlinearity=lasagne.nonlinearities.rectify)
+    print(lasagne.layers.get_output_shape(network))
+    
+    
+    network = lasagne.layers.Conv2DLayer(
+    network, num_filters=32, filter_size=(4, 4),
+    nonlinearity=lasagne.nonlinearities.rectify)
+    print(lasagne.layers.get_output_shape(network))
+    
+    network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
     print(lasagne.layers.get_output_shape(network))
 
     network = lasagne.layers.DenseLayer(network,
@@ -197,32 +219,39 @@ def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
         
 def main():    
     images = read_data()
-    num_epochs = 10
+    num_epochs = 100
     data, labels = create_dataset(images)
+    data -= np.mean(data)
+    data = data / np.std(data)
     X_train, y_train, X_val, y_val, X_test, y_test = data_split(data,labels)
     #Create training functions in theano
     input_var = T.tensor4('inputs')
-    target_var = T.imatrix('targets')
+    target_var = T.ivector('targets')
     network = build_cnn(input_var)
     
+    
+    epsilon = 1e-9
+    
     prediction = lasagne.layers.get_output(network)
-    prediction = T.clip(prediction,0.00000001,0.99999999)
+    prediction = T.clip(prediction,epsilon,1-epsilon)
     loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
     loss = loss.mean()
 
     params = lasagne.layers.get_all_params(network, trainable=True)
     updates = lasagne.updates.nesterov_momentum(
-    loss, params, learning_rate=0.001, momentum=0.9)
+    loss, params, learning_rate=0.0001, momentum=0.9)
 
     test_prediction = lasagne.layers.get_output(network, deterministic=True)
-    test_prediction = T.clip(prediction,0.00000001,0.99999999)
+    test_prediction = T.clip(prediction,epsilon,1-epsilon)
     test_loss = lasagne.objectives.categorical_crossentropy(test_prediction,
     target_var)
     test_loss = test_loss.mean()
+    test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var),
+                      dtype=theano.config.floatX)
 
     train_fn = theano.function([input_var, target_var], [loss,prediction], updates=updates)
 
-    val_fn = theano.function([input_var, target_var], [test_loss, test_prediction])
+    val_fn = theano.function([input_var, target_var], [test_loss, test_acc])
     
     # Finally, launch the training loop.
     print("Starting training...")
@@ -233,38 +262,40 @@ def main():
         rd_err = 0
         train_batches = 0
      
-        for batch in iterate_minibatches(X_train, y_train, 100, shuffle=True):
+        for batch in iterate_minibatches(X_train, y_train, 500, shuffle=True):
             inputs, targets = batch
             rd_err, preds = train_fn(inputs, targets)
             train_batches += 1
             train_err += rd_err
             
-            if(train_batches % 10 == 0):
-                print("Completed training minibatch {}".format(train_batches))
             
             
         # And a full pass over the validation data:
         val_err = 0
         val_batches = 0
-        for batch in iterate_minibatches(X_val, y_val, 100, shuffle=False):
+        val_acc = 0
+        for batch in iterate_minibatches(X_val, y_val, 500, shuffle=False):
             inputs, targets = batch
-            err, preds = val_fn(inputs, targets)
+            err, acc = val_fn(inputs, targets)
             val_err += err
+            val_acc += acc
             val_batches += 1
         # Then we print the results for this epoch:
         print("Epoch {} of {} Completed".format(
         epoch + 1, num_epochs))
         print(" training loss:\t\t{:.6f}".format(train_err / train_batches))
         print(" validation loss:\t\t{:.6f}".format(val_err / val_batches))
-        # After training, we compute and print the test error:
-        test_err = 0
-        test_batches = 0
-        for batch in iterate_minibatches(X_test, y_test, 100, shuffle=False):
-            inputs, targets = batch
-            err, preds = val_fn(inputs, targets)
-            test_err += err
-            test_batches += 1
-        print("Final results:")
-        print(" test loss:\t\t\t{:.6f}".format(test_err / test_batches))
+        print(" validation accuracy:\t\t{:.2f} %".format(
+                 val_acc / val_batches * 100))
+    # After training, we compute and print the test error:
+    test_err = 0
+    test_batches = 0
+    for batch in iterate_minibatches(X_test, y_test, 500, shuffle=False):
+        inputs, targets = batch
+        err, preds = val_fn(inputs, targets)
+        test_err += err
+        test_batches += 1
+    print("Final results:")
+    print(" test loss:\t\t\t{:.6f}".format(test_err / test_batches))
 main()
 
